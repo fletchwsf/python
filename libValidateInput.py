caf101fadd1a84795c5554588f71a5114180b64d
wsf <wsf.fletcher@gmail.com>
2015-09-02 20:03:16 -0400

    start a user input validation library

---
+++ libValidateInput.py
@@ -0,0 +1,26 @@
+#------------------------------------------------------------
+# Range checks
+#------------------------------------------------------------
+
+# check for n greater than (x)
+def greaterThan(n,x)
+    if ( n > x):
+        return n
+    #else
+    print "You entered:" + str(n)
+    print "Enter value greater than" + str(x)
+    return(null) 
+
+# check for n between (x,y)
+def rangeBetween(n,x,y ):
+    if ( (x < n) and ( n < y) ):
+        return(n)
+    #else
+    print "You entered:" + str(n)
+    print "Enter value between " + str(x) +  " and " + str(y)
+    return(null) 
+
+#------------------------------------------------------------
+# Type checks
+
+# check for integer type
